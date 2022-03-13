3ab59b9b736b7cb13cc9621413196fb8970844de
angeldragiew <angeldragieww@gmail.com>
2022-03-13 15:48:47 +0200

    Added IRepository interface and implemented Repository class for working with database

---
+++ BestFood.Infrastructure/Data/Repo/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestFood.Infrastructure.Data.Repo
+{
+    public interface IRepository<T>
+        where T : class
+    {
+        IQueryable<T> All();
+
+        void AddAsync(T entity);
+
+        Task AddRangeAsync(IEnumerable<T> entities);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+        void DeleteRange(IEnumerable<T> entities);
+
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ BestFood.Infrastructure/Data/Repo/Repository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BestFood.Infrastructure.Data.Repo
+{
+    public class Repository<T> : IRepository<T>, IDisposable
+        where T : class
+    {
+        private readonly ApplicationDbContext context;
+        private readonly DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<T>();
+        }
+        public void AddAsync(T entity)
+        {
+            dbSet.AddAsync(entity);
+        }
+
+        public Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            return dbSet.AddRangeAsync(entities);
+        }
+
+        public IQueryable<T> All()
+        {
+            return this.dbSet;
+        }
+
+        public void Delete(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            return context.SaveChangesAsync();
+        }
+
+        public void Update(T entity)
+        {
+            dbSet.Update(entity);
+        }
+
+        public void Dispose()
+        {
+            this.context.Dispose();
+        }
+    }
+}
