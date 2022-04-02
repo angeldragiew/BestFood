d6a6f055e86e2d531932f88776a1ea9c21b335e3
angeldragiew <angeldragieww@gmail.com>
2022-04-02 15:54:15 +0300

    Redirect to home page after logout

--- BestFoodWebApp/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ BestFoodWebApp/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -28,16 +28,16 @@ namespace BestFoodWebApp.Areas.Identity.Pages.Account
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
-            if (returnUrl != null)
-            {
-                return LocalRedirect(returnUrl);
-            }
-            else
-            {
+            //if (returnUrl != null)
+            //{
+                return LocalRedirect("/Home/Index");
+            //}
+            //else
+            //{
                 // This needs to be a redirect so that the browser performs a new
                 // request and the identity for the user gets updated.
-                return RedirectToPage();
-            }
+            //    return RedirectToPage();
+            //}
         }
     }
 }
