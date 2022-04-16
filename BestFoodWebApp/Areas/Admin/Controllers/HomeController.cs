dfc4b82623f8e1d5d5eca3da15b641c1e2615ac7
angeldragiew <angeldragieww@gmail.com>
2022-04-16 10:33:51 +0300

    Added admin and user layout

---
+++ BestFoodWebApp/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BestFoodWebApp.Areas.Admin.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public IActionResult AdminArea()
+        {
+            return View();
+        }
+    }
+}
