8c56d74a2e5bac66d20b126653b5c12f8032f3d3
angeldragiew <angeldragieww@gmail.com>
2022-04-29 19:20:56 +0300

    Minor changes

---
+++ BestFoodWebApp/wwwroot/js/comment.js
@@ -0,0 +1,13 @@
+﻿let input = document.querySelector(".commentInput");
+let button = document.querySelector(".commentButton");
+button.disabled = true;
+
+input.addEventListener("input", stateHandle);
+
+function stateHandle() {
+    if (document.querySelector(".commentInput").value === "") {
+        button.disabled = true;
+    } else {
+        button.disabled = false;
+    }
+}
---
+++ BestFoodWebApp/wwwroot/js/rejectNote.js
@@ -0,0 +1,15 @@
+﻿let input = document.querySelector(".RejectOrderNote");
+let button = document.querySelector(".reject-button");
+button.disabled = true;
+
+input.addEventListener("input", stateHandle);
+
+function stateHandle() {
+    if (document.querySelector(".RejectOrderNote").value.length<5) {
+        button.disabled = true;
+    } else {
+        button.disabled = false;
+    }
+}
+
+
